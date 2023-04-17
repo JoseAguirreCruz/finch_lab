8643e3b85981a889843fe52702a55bfa0416a4dc
Jose Aguirre <aguirrejose615@gmail.com>
2023-04-17 14:34:02 -0400

    done

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -1,11 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
-class Finch(models.Model):
-      name = models.CharField(max_length=100)
-      breed = models.CharField(max_length=100)
-      description = models.CharField(max_length=250)
-      age = models.IntegerField()
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
-      def __str__(self):
-          return self.name
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+)
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
